303fcd80509518e4b6dddc5c6df2abbdda7df463
GSergeevich <germansergeevich1@yandex.ru>
2020-10-29 10:31:24 +0300

    Исправлены замечания к 3 задаче

--- Ruby_basic/lesson1/triangle_type.rb
+++ Ruby_basic/lesson1/triangle_type.rb
@@ -2,15 +2,21 @@
 
 puts 'Введите три стороны треугольника - a,b,c:'
 sides = Array.new(gets.chomp.split(','))
-a,b,c = *sides
+a,b,c = *sides.map!(&:to_f)
 
 print "Треугольник является "
 
-if a == b && b == c 
-	puts "равносторонним."
+# hyp_pyp mean a "hypotetical hypotenuse"
+hyp_hyp = sides.sort![-1]
+
+if hyp_hyp ** 2 == sides[0] ** 2 + sides[1] ** 2
+  puts "прямоугольным." 
+
+elsif a == b && b == c 
+  puts "равносторонним и равнобедренным."
+
 elsif
-   a == b || b == c
-   puts "равнобедренным."
-else
-   puts "прямоугольным." 
+  a == b || b == c || a == c
+  puts "равнобедренным."
+
 end
