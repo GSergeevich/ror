a9db096b62638c3f5d2dfa36778d2b10f1e99519
GSergeevich <germansergeevich1@yandex.ru>
2020-11-10 10:14:29 +0300

    Решение задания к уроку 3

---
+++ Ruby_basic/lesson3/demo.rb
@@ -0,0 +1,87 @@
+# /usr/bin/env ruby
+
+require_relative 'railroad'
+
+station1 = Station.new('Lud')
+station2 = Station.new('Candleton')
+station3 = Station.new('Rilea')
+station4 = Station.new('Dasherville')
+station5 = Station.new('Topeka')
+
+r1 = Route.new(station1,station5)
+
+puts "Добавляем/удаляем станции в маршрут"
+
+r1.stations.each {|st| puts st.title}
+r1.insert(station2)
+puts '+++'
+r1.stations.each {|st| puts st.title}
+r1.insert(station3)
+puts '+++'
+r1.stations.each {|st| puts st.title}
+r1.insert(station4)
+puts '+++'
+r1.stations.each {|st| puts st.title}
+r1.delete(station4)
+puts '---'
+r1.stations.each {|st| puts st.title}
+r1.insert(station4)
+puts '+++'
+r1.stations.each {|st| puts st.title}
+puts '---'
+
+puts "Определяем поезд:"
+blaine_the_mono = Train.new(1,'passengers',4)
+p blaine_the_mono.number
+
+puts '---'
+puts "Принимаем маршрут: #{blaine_the_mono.route(r1)}"
+
+puts '---'
+puts "Играем с вагонами:"
+
+puts "Вагонов: #{blaine_the_mono.carriages}"
+puts "Прицепили вагон: #{blaine_the_mono.attach}"
+puts "Вагонов: #{blaine_the_mono.carriages}"
+puts "Отцепили вагон: #{blaine_the_mono.detach}"
+puts "Вагонов: #{blaine_the_mono.carriages}"
+
+puts '---'
+puts "Поехали:"
+
+puts "Скорость: #{blaine_the_mono.speed} => #{blaine_the_mono.speed= 1225}"
+
+puts '---'
+
+puts "Играем с вагонами на скорости"
+puts "Вагонов: #{blaine_the_mono.carriages}"
+puts "Прицепили вагон: #{blaine_the_mono.attach}"
+puts "Вагонов: #{blaine_the_mono.carriages}"
+puts "Отцепили вагон: #{blaine_the_mono.detach}"
+puts "Вагонов: #{blaine_the_mono.carriages}"
+
+puts "Приехали на следующую станцию: #{blaine_the_mono.forward}"
+puts '---'
+puts "Предыдущая,текущая и следующая станции: #{blaine_the_mono.near_stations}"
+puts '---'
+puts "Приехали на следующую станцию: #{blaine_the_mono.forward}"
+puts "-----"
+puts "Приехали на следующую станцию: #{blaine_the_mono.forward}"
+puts "-----"
+
+puts "Сброс скорости"
+puts "#{blaine_the_mono.speed} => #{blaine_the_mono.speed= 0 }"
+puts "-----"
+
+puts "Набор скорости"
+puts "#{blaine_the_mono.speed} => #{blaine_the_mono.speed= 1225 }"
+puts "-----"
+
+puts "Приехали на следующую станцию: #{blaine_the_mono.forward}"
+puts "-----"
+
+puts "Набор скорости"
+puts "#{blaine_the_mono.speed= 1500} => #{blaine_the_mono.speed= 1700}"
+puts "Сброс скорости"
+puts "#{blaine_the_mono.speed} => #{blaine_the_mono.speed= 0 }"
+puts "Приехали"
---
+++ Ruby_basic/lesson3/railroad.rb
@@ -0,0 +1,98 @@
+#! /usr/bin/env ruby
+
+class Station
+    
+  attr_reader :title
+
+	def initialize(title)
+      @title = title
+      @depot = {cargo: [],passengers: []}
+	end
+
+	def receive(train)
+		@depot[train.type.to_sym] << train
+	end
+
+	def send(train)
+		@depot[train.type.to_sym].delete(train)
+	end
+
+	def trains
+		@depot.each {|type,trains| puts trains}
+	end
+
+	def trains_number
+		puts "Грузовых: #{@depot[:cargo].length}"
+		puts "Пассажирских: #{@depot[:passengers].length}"
+	end
+
+end
+
+class Route
+	
+	attr_reader :stations
+
+	def initialize(first_station,last_station)
+      @stations = first_station,last_station
+    end
+
+	def insert(station)
+		@stations.insert(-2,station)
+	end
+
+	def delete(station)
+		@stations.delete(station) if @stations.include?(station)
+	end
+
+end
+
+class Train
+  
+  attr_accessor :speed
+  attr_reader :carriages, :type , :number
+
+  def initialize(number,type,carriages)
+  	@number= number.to_s
+  	@type = type # 'cargo' or 'passengers'
+  	@carriages = carriages
+    @speed = 0
+  end
+
+  def detach
+    @carriages -= 1 if @speed == 0 
+    @carriages
+  end
+
+  def attach
+    @carriages += 1 if @speed == 0
+    @carriages
+  end
+
+  def route(route)
+  	@route = route
+  	@current_station_index = 0 
+  	@route.stations[@current_station_index].receive(self)
+        @route.stations.map {|st| st.title}
+  end
+
+  def forward
+  	@route.stations[@current_station_index].send(self)
+  	@current_station_index += 1
+  	@route.stations[@current_station_index].receive(self)
+    @route.stations[@current_station_index].title
+  end
+
+  def backward
+  	@route.stations[@current_station_index].send(self)
+  	@current_station_index += 1
+  	@route.stations[@current_station_index].recieve(self)
+    @route.stations[@current_station_index].title
+  end
+
+  def near_stations
+    index = @current_station_index 
+    @route.stations[(index - 1)..(index + 1)].map {|station|  station.title }
+  end
+
+
+end
